f27c854aa2caac17f928a1576f9e8419bee99602
vkhomits <vkhomits@anaconda.com>
2026-09-09 13:35:50 +0300

    Update version to 2.42.3 (#3)
    * Update version to 2.42.3
    
    ---------
    
    Co-authored-by: vkhomits <khomitsvalia@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -37,6 +37,13 @@ known_fail="TS_OPT_misc_setarch_known_fail=yes"
 known_fail+=" TS_OPT_column_invalid_multibyte_known_fail=yes"
 known_fail+=" TS_OPT_hardlink_options_known_fail=yes"  # flaky on py3.9?
 known_fail+=" TS_OPT_uuid_oids_known_fail=yes"
+# stale libuuid.so.1 in build-time library path lacks the UUID_2.41
+# symbol version exported by the freshly-built lib, since these tests
+# run before `make install`; ELF symbol versioning makes this Linux-only
+known_fail+=" TS_OPT_uuidgen_oids_known_fail=yes"
+known_fail+=" TS_OPT_uuidgen_uuidgen_known_fail=yes"
+known_fail+=" TS_OPT_uuidparse_time_known_fail=yes"
+known_fail+=" TS_OPT_uuidparse_uuidparse_known_fail=yes"
 
 if [[ $target_platform == linux-aarch64 ]]; then
   known_fail+=" TS_OPT_lsfd_mkfds_ro_regular_file_known_fail=yes"  # can be flaky on this platform
@@ -50,6 +57,7 @@ if [[ $target_platform == linux-aarch64 ]]; then
   known_fail+=" TS_OPT_lsfd_option_inet_known_fail=yes"
   # script/options fails on pypy + aarch64 under emulation
   known_fail+=" TS_OPT_script_options_known_fail=yes"
+  known_fail+=" TS_OPT_fincore_count_known_fail=yes"
 fi
 
 if [[ $target_platform != osx-* ]]; then
